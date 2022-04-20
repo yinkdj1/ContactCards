5b67ce7fca96ec58f5c80592f3f85d610384caa4
yinkdj1 <101015317+yinkdj1@users.noreply.github.com>
2022-04-20 00:31:44 -0500

    add mapping

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+
 import Avatar from "./Avatar";
 import Detail from "./Detail";
 
@@ -6,6 +7,7 @@ function Card(props) {
   return (
     <div className="card">
       <div className="top">
+        <p> {props.id} </p>
         <h2 className="name">{props.name}</h2>
         <Avatar img={props.img} />
       </div>
